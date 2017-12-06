bd802c4c9e72de1c841c7137ed75e13822f632c1
larce <luis.arce22@gmail.com>
2017-12-05 22:13:46 -0400

    Added home page to show the basic info of profiles lists

---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <b-container>
+      <b-card v-for="profile in profiles" :title="profile.name"
+        :img-src="profile.src" img-top tag="article" style="max-width: 30rem;" class="mb-4"
+       >
+        <p class="card-text">
+          {{ profile.description }}
+        </p>
+      </b-card>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      profiles: [
+        {
+          src: "https://uploads.toptal.io/blog/hiring_guide/image/2/1_A.png", 
+          name: "Full Stack Python Developer",
+          description: "I work with python in a ecommerce project by more than one year, I used jinja, javascript, jquery and boostrap with this project combined with python. I love how python simplifies the code."
+        },
+        {
+          src: "https://udemy-images.udemy.com/course/480x270/607218_a05a_2.jpg", 
+          name: "Full Stack JavaScript Developer",
+          description: "I work with JavaScript project by more thatn 2 years, I have some personal project where I could use javascript framework like angular, reactjs"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,31 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import bModal from 'bootstrap-vue/es/components/modal/modal'
+import bModalDirective from 'bootstrap-vue/es/directives/modal/modal'
+
+// This imports <b-modal> as well as the v-b-modal directive as a plugin:
+import { Modal } from 'bootstrap-vue/es/components'
+
+// This imports <b-card> along with all the <b-card-*> sub-components as a plugin:
+import { Card } from 'bootstrap-vue/es/components'
+
+// This imports directive v-b-scrollspy as a plugin:
+import { Scrollspy } from 'bootstrap-vue/es/directives'
+
 
 Vue.config.productionTip = false
+
+Vue.use(Modal)
+Vue.use(Card)
+Vue.use(Scrollspy)
+Vue.use(BootstrapVue)
+Vue.component('b-modal', bModal)
+Vue.directive('b-modal', bModalDirective)
 
 /* eslint-disable no-new */
 new Vue({
