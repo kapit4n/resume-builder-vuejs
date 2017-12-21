8ba91767056a22b437e1425863421be6b5ab56ae
larce <luis.arce22@gmail.com>
2017-12-20 22:40:53 -0400

    Use routes to redirecto to profiles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@
       <b-card v-for="profile in profiles" :title="profile.name"
         :img-src="profile.src" img-top tag="article" style="max-width: 300px; display: inline-grid;" class="mb-4"
        >
-       <router-link to="/Resume/1" tag="button">GO</router-link>
+       <router-link :to="{ name: 'Resume', params: { id: profile.id }}" tag="button">GO</router-link>
         <p class="card-text">
           {{ profile.description }}
         </p>
@@ -21,31 +21,37 @@ export default {
     return {
       profiles: [
         {
+          id: 1,
           src: 'https://customjavadevelopment.files.wordpress.com/2015/11/which-is-better-java-or-scala.jpg',
           name: 'Full Stack Scala Developer',
           description: 'I work with python in a ecommerce project by more than one year, I used jinja, javascript, jquery and boostrap with this project combined with python. I love how python simplifies the code.'
         },
         {
+          id: 2,
           src: 'https://s3-ap-southeast-1.amazonaws.com/work.smartcasual.co/opportunities/header_imgs/33/medium.png',
           name: 'Full Stack Java Developer',
           description: 'I work with JavaScript project by more thatn 2 years, I have some personal project where I could use javascript framework like angular, reactjs'
         },
         {
+          id: 3,
           src: 'https://uploads.toptal.io/blog/hiring_guide/image/2/1_A.png',
           name: 'Full Stack Python Developer',
           description: 'I work with python in a ecommerce project by more than one year, I used jinja, javascript, jquery and boostrap with this project combined with python. I love how python simplifies the code.'
         },
         {
+          id: 4,
           src: 'https://udemy-images.udemy.com/course/480x270/607218_a05a_2.jpg',
           name: 'Full Stack JavaScript Developer',
           description: 'I work with JavaScript project by more thatn 2 years, I have some personal project where I could use javascript framework like angular, reactjs'
         },
         {
+          id: 5,
           src: 'https://bs-uploads.toptal.io/blackfish-uploads/job_description_page/seo/og_image_file/og_image/1804/angular_job-01-eca8007a71c629ae2e8666a022150fa6.jpg',
           name: 'Angular js Developer',
           description: 'I work with Angular project by more thatn 2 years, I have some personal project where I could use javascript framework like angular, reactjs'
         },
         {
+          id: 6,
           src: 'https://qph.ec.quoracdn.net/main-qimg-ba62e642e112dc7ec2d1043faeb1a560',
           name: 'Reacj js Developer',
           description: 'I worked in personal projects'
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -2,21 +2,21 @@
   <div class="resume">
     <b-container class="bv-example-row">
 
-   <b-card :title="profile.name"
+   <b-card :title="profiles[index].name"
         tag="article"
         >
-    <img :src="profile.src" width="50" >
+    <img :src="profiles[index].src" width="50" >
     <h3 style="text-align: left;">Summary {{$route.params.id}}</h3>
-    <p class="card-text" style="text-align: left;" v-html="profile.summary">
+    <p class="card-text" style="text-align: left;" v-html="profiles[index].summary">
     </p>
     <h3 style="text-align: left;">Education</h3>
-    <p class="card-text" style="text-align: left;" v-html="profile.skills">
+    <p class="card-text" style="text-align: left;" v-html="profiles[index].skills">
     </p>
     <h3 style="text-align: left;">Employment History</h3>
-    <p class="card-text" style="text-align: left;" v-html="profile.history">
+    <p class="card-text" style="text-align: left;" v-html="profiles[index].history">
     </p>
     <h3 style="text-align: left;">Hobbies and Interest</h3>
-    <p class="card-text" style="text-align: left;" v-html="profile.hobbies">
+    <p class="card-text" style="text-align: left;" v-html="profiles[index].hobbies">
     </p>
     <b-button href="#" variant="primary">Edit</b-button>
   </b-card>
@@ -29,10 +29,12 @@ export default {
   name: 'Resume',
   data () {
     return {
-      msg: this.$route.params.id,
-      profile: {
+      msg: this.name,
+      index: this.$route.params.id % 3,
+      profiles: [
+      {
         src: 'https://customjavadevelopment.files.wordpress.com/2015/11/which-is-better-java-or-scala.jpg',
-        name: 'Full Stack Software Developer',
+        name: 'Full Stack Software Developer 000',
         description: 'I work with python in a ecommerce project by more than one year, I used jinja, javascript, jquery and boostrap with this project combined with python. I love how python simplifies the code.',
         summary: `I have been working on software development professionally since 2004. I like working on web application development, especially on systems requiring complex backends and cryptography. I also have fun working on projects which communicate with or run on custom hardware.<br>
 
@@ -43,13 +45,53 @@ export default {
       - Embedded systems and appliances with custom hardware<br>`,
         hobbies: `I am a quick learner and I really enjoy learning something new on every project I work on. The more challenging the project is, the more I enjoy working on it. less`,
         skills: `- Website development, both front-end (HTML/CSS/JS) and backend (Rails/Elixir/PHP)<br>
-      - Desktop application development in Java/JavaFX<br>
-      - Mobile application development for Android, iPhone<br>
-      - Knowledge of cryptographic schemes for authentication, signing, encryption, etc<br>
-      - Development of Javacard based applets<br>
-      - Linux system administration<br>`
+        - Desktop application development in Java/JavaFX<br>
+        - Mobile application development for Android, iPhone<br>
+        - Knowledge of cryptographic schemes for authentication, signing, encryption, etc<br>
+        - Development of Javacard based applets<br>
+        - Linux system administration<br>`
+        },
+        {
+        src: 'https://customjavadevelopment.files.wordpress.com/2015/11/which-is-better-java-or-scala.jpg',
+        name: 'Full Stack Software Developer 001',
+        description: 'I work with python in a ecommerce project by more than one year, I used jinja, javascript, jquery and boostrap with this project combined with python. I love how python simplifies the code.',
+        summary: `I have been working on software development professionally since 2004. I like working on web application development, especially on systems requiring complex backends and cryptography. I also have fun working on projects which communicate with or run on custom hardware.<br>
+
+      I have been working in the telecommunication industry and for 5 years I was employed by a large German SmartCard manufacturer.`,
+        history: `- Web site and web application development based on Ruby on Rails and recently Elixir/Phoenix<br>
+      - Desktop software for Mac and Windows, mostly PDF processing tools<br>
+      - Enterprise systems in the Telecommunication industry, based on J2EE technologies<br>
+      - Embedded systems and appliances with custom hardware<br>`,
+        hobbies: `I am a quick learner and I really enjoy learning something new on every project I work on. The more challenging the project is, the more I enjoy working on it. less`,
+        skills: `- Website development, both front-end (HTML/CSS/JS) and backend (Rails/Elixir/PHP)<br>
+        - Desktop application development in Java/JavaFX<br>
+        - Mobile application development for Android, iPhone<br>
+        - Knowledge of cryptographic schemes for authentication, signing, encryption, etc<br>
+        - Development of Javacard based applets<br>
+        - Linux system administration<br>`
+        },
+        {
+        src: 'https://customjavadevelopment.files.wordpress.com/2015/11/which-is-better-java-or-scala.jpg',
+        name: 'Full Stack Software Developer 002',
+        description: 'I work with python in a ecommerce project by more than one year, I used jinja, javascript, jquery and boostrap with this project combined with python. I love how python simplifies the code.',
+        summary: `I have been working on software development professionally since 2004. I like working on web application development, especially on systems requiring complex backends and cryptography. I also have fun working on projects which communicate with or run on custom hardware.<br>
+
+      I have been working in the telecommunication industry and for 5 years I was employed by a large German SmartCard manufacturer.`,
+        history: `- Web site and web application development based on Ruby on Rails and recently Elixir/Phoenix<br>
+      - Desktop software for Mac and Windows, mostly PDF processing tools<br>
+      - Enterprise systems in the Telecommunication industry, based on J2EE technologies<br>
+      - Embedded systems and appliances with custom hardware<br>`,
+        hobbies: `I am a quick learner and I really enjoy learning something new on every project I work on. The more challenging the project is, the more I enjoy working on it. less`,
+        skills: `- Website development, both front-end (HTML/CSS/JS) and backend (Rails/Elixir/PHP)<br>
+        - Desktop application development in Java/JavaFX<br>
+        - Mobile application development for Android, iPhone<br>
+        - Knowledge of cryptographic schemes for authentication, signing, encryption, etc<br>
+        - Development of Javacard based applets<br>
+        - Linux system administration<br>`
+        },
+
+      ],
       }
-    }
   }
 }
 </script>
